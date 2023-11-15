e125656f890e13e49bc3ca49665665d66c825c42
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-16 00:07:28 +0330

    craete SectionHeader

---
+++ src/Components/Modules/SectionHeader/SectionHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SectionHeader({title}) {
+  return (
+    <div className=" mt-5 ">
+        <h4 className=' fw-bold '>{title}</h4>
+    </div>
+  )
+}
