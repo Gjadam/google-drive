dba55b6077736fa5601c62a3423013c5a75934f4
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-16 00:07:12 +0330

    create fileBox

---
+++ src/Components/Modules/FileBox/FileBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './FileBox.css'
+import { TbPdf } from "react-icons/tb";
+export default function FileBox() {
+  return (
+    <div className='file-box__size d-flex justify-content-center align-items-center flex-column p-3 rounded-5 '>
+        <img src="/images/svgs/logo.svg" className='file-box-image' alt="image" />
+        <div className="file-box__content d-flex justify-content-between w-100">
+            <h6 className=' text-black '>file.pdf</h6>
+            <TbPdf className=' bg-primary text-white rounded-3 px-1 fs-3 '/>
+        </div>
+    </div>
+  )
+}
