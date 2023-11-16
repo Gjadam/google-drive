a6bf05b8c67f049cbed8d64d267e1f9c06e748b1
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-16 16:15:27 +0330

    create All Files section

--- src/Pages/Index/Index.js
+++ src/Pages/Index/Index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Index.css'
-import { ButtonGroup, Container, Dropdown } from 'react-bootstrap'
+import { ButtonGroup, Container, Dropdown, Table } from 'react-bootstrap'
 import { HiPlus } from "react-icons/hi";
 import { MdOutlineCreateNewFolder } from "react-icons/md";
 import NavBar from '../../Components/Templates/NavBar/NavBar'
@@ -15,7 +15,7 @@ export default function Index() {
       {/* Start Add Drive */}
       <div className="d-flex align-items-center mt-5">
         <h1 className=' fw-bold '>My Drive</h1>
-        <form className='ms-3 ' method="POST" enctype="multipart/form-data">
+        <form className='ms-3 mb-1  ' method="POST" enctype="multipart/form-data">
           <Dropdown as={ButtonGroup}>
             <label for="files" className='btn btn-primary d-flex align-items-center  '><HiPlus /></label>
             <input id="files" className='d-none' type="file" />
@@ -28,15 +28,59 @@ export default function Index() {
       </div>
       {/* End Add Drive */}
       <SectionHeader title="Recent Files" />
-      {/* Start File Boxes */}
+      {/* Start Recent Files */}
       <div className=" d-flex justify-content-around align-items-centerf flex-wrap gap-3 mt-4  ">
         <FileBox />
         <FileBox />
         <FileBox />
         <FileBox />
       </div>
-      {/* End File Boxes */}
+      {/* End Recent Files */}
       <SectionHeader title="All Files" />
+      {/* Start All Files  */}
+      <div>
+        <Table responsive className='table-borderless'>
+          <thead>
+            <tr >
+              <th className='text-black-50'>Name</th>
+              <th className='text-black-50'>Last Modified</th>
+              <th className='text-black-50'>Size</th>
+              <th className='text-black-50'>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img src="/images/png/pdf-icon.png" alt="icon" />
+                <span className=' ms-2 '>pdf file test</span>
+              </td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+
+            </tr>
+            <tr>
+              <td>
+                <img src="/images/png/pdf-icon.png" alt="icon" />
+                <span className=' ms-2 '>pdf file test</span>
+              </td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="/images/png/pdf-icon.png" alt="icon" />
+                <span className=' ms-2 '>pdf file test</span>
+              </td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      {/* End All Files  */}
 
     </Container>
   )
