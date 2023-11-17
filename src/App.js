d282662d65b8874d559430046210e03e970d7690
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-17 16:48:51 +0330

    Add some Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { useRoutes } from 'react-router-dom';
 import './App.css';
 import routes from './routes';
+import NavBar from './Components/Templates/NavBar/NavBar';
+import { Container } from 'react-bootstrap';
+import PageStyle from './Components/Templates/PageStyle/PageStyle';
 function App() {
   let router = useRoutes(routes)
   return (
-    <>
+    <Container>
+      <NavBar />
+      <PageStyle />
       {router}
-    </>
+    </Container>
   );
 }
 
