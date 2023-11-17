7b56f226d93ec96dba99fb26813e8244faff54f8
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-17 23:57:21 +0330

    landing page sections

---
+++ src/Components/Modules/LandingPageSection/LandingPageSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './LandingPageSection.css'
+import { Container } from 'react-bootstrap'
+export default function LandingPageSection({title, text, image, dir}) {
+  return (
+    <div className={`${dir === 'reverse' ? 'flex-row-reverse ' : 'flex-row'} row  d-flex justify-content-center align-items-center flex-wrap  mt-5 m-auto `}>
+        <div className="col d-flex mb-4  mb-sm-0   ">
+            <img src={image} alt="image" className='landing__image-height m-auto' />
+        </div>
+        <div className="col d-flex flex-column align-items-center  ">
+            <h2 className='landing-page-section__title'>{title}</h2>
+            <h6  className='landing-page-section__text lh-base  text-black-50 '>{text}</h6>
+        </div>
+    </div>
+
+  )
+}
