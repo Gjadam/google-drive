8a99614fba1671e4e847ba026936ec10800db3e8
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-17 16:49:02 +0330

    add new route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
+import FileInfo from "./Pages/FileInfo/FileInfo"
 import Index from "./Pages/Index/Index"
+import NotFound from "./Pages/NotFound/NotFound"
 
 
 const routes = [
     { path: '/', element: <Index />},
+    { path: '/file-info/:fileName', element: <FileInfo /> },
+    { path: '*', element: <NotFound /> },
 ]
 
 export default routes
