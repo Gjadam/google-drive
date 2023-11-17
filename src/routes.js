06b98c8390a5850cf3149f60af78a1356621c7d4
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-17 23:56:50 +0330

    add some route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import FileInfo from "./Pages/FileInfo/FileInfo"
 import Index from "./Pages/Index/Index"
+import LandingPage from "./Pages/LandingPage/LandingPage"
 import NotFound from "./Pages/NotFound/NotFound"
+import SignUp from "./Pages/SignUp/SignUp"
 
 
 const routes = [
-    { path: '/', element: <Index />},
+    { path: '/', element: <LandingPage />},
+    { path: '/fast-drive', element: <Index />},
     { path: '/file-info/:fileName', element: <FileInfo /> },
+    { path: '/sign-up', element: <SignUp /> },
     { path: '*', element: <NotFound /> },
 ]
 
