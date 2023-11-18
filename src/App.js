60709194b9d6f62f8407b32421e43a1dcd644d9d
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-18 17:28:53 +0330

    create AuthContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import { useRoutes } from 'react-router-dom';
 import './App.css';
+import AuthContext from './context/authContext';
 import routes from './routes';
+import { useState } from 'react';
 function App() {
-  let router = useRoutes(routes)
-  return (
-    <>
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [token, setToken] = useState(false)
+  const [userInfos, setUserInfos] = useState({})
 
+  const router = useRoutes(routes)
+
+  const login = (token) => {
+    setToken(token)
+    localStorage.setItem('user', JSON.stringify({ token }))
+  }
+
+  const logout = () => {
+    setToken(null)
+    setUserInfos({})
+    localStorage.removeItem('user')
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      isLoggedIn,
+      token,
+      userInfos,
+      login,
+      logout,
+    }}>
       {router}
-    </>
+    </AuthContext.Provider>
 
   );
 }
---
+++ src/context/authContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+
+const AuthContext = createContext({
+    isLoggedIn: false,
+    token: null,
+    userInfos: null,
+    login: () => {},
+    logout: () => {},
+})
+
+
+export default AuthContext
