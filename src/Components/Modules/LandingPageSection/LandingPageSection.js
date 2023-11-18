8ed53852f8fe77656a9329997fbd4e375c070985
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-18 18:32:19 +0330

    change AOS style

--- src/Components/Modules/LandingPageSection/LandingPageSection.js
+++ src/Components/Modules/LandingPageSection/LandingPageSection.js
@@ -3,7 +3,7 @@ import './LandingPageSection.css'
 import { Container } from 'react-bootstrap'
 export default function LandingPageSection({title, text, image, dir}) {
   return (
-    <div className={`${dir === 'reverse' ? 'flex-row-reverse ' : 'flex-row'} row  d-flex justify-content-center align-items-center flex-wrap  mt-5 m-auto `} data-aos="fade-right" data-aos-duration="1500">
+    <div className={`${dir === 'reverse' ? 'flex-row-reverse ' : 'flex-row'} row  d-flex justify-content-center align-items-center flex-wrap  mt-5 m-auto `} data-aos="fade-up" data-aos-duration="1500">
         <div className="col d-flex mb-4  mb-sm-0   ">
             <img src={image} alt="image" className='landing__image-section-height m-auto' />
         </div>
