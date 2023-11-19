0b641abc4d483fb07fd85a5bdf72b1837f5b2782
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-20 00:07:11 +0330

    add Accordion Box

---
+++ src/Components/Modules/AccordionBox/AccordionBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Accordion } from 'react-bootstrap'
+
+export default function AccordionBox({ eventKey, header, body }) {
+    return (
+        <>
+            <Accordion.Item eventKey={eventKey}>
+                <Accordion.Header className=''>{header}</Accordion.Header>
+                <Accordion.Body className=' lh-lg '>{body}</Accordion.Body>
+            </Accordion.Item>
+        </>
+    )
+}
