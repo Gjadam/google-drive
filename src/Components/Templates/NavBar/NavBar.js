e2441894c104e46440f0ada32829bfe0c726fe87
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-19 12:21:19 +0330

    change navbar

--- src/Components/Templates/NavBar/NavBar.js
+++ src/Components/Templates/NavBar/NavBar.js
@@ -3,52 +3,64 @@ import { Button, Container, Form, Image, Nav, NavDropdown, Navbar, Offcanvas } f
 import { FaCircleUser } from "react-icons/fa6";
 import './NavBar.css'
 import AuthContext from '../../../context/authContext';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 export default function NavBar() {
 
   const authContext = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const logOut = () => {
+    Swal.fire({
+      title: "Do you want to Log out?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "You have successfully logged out.",
+          icon: "success"
+        }).then(() => {
+          authContext.logout()
+          navigate('/')
+        })
+      }
+    });
+  }
 
   return (
     <>
-      {['lg'].map((expand) => (
-        <Navbar key={expand} expand={expand} className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#">
-              <div className="">
-                <img src="/images/svgs/logo.svg" alt="logo" />
-              </div>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="start"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  <div className="">
-                    <img src="/images/svgs/logo.svg" alt="logo" />
-                  </div>
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Form className="d-flex">
+      <Navbar expand="lg" className=" bg-white ">
+        <Container>
+          <Navbar.Brand href="#home">
+            <div className="">
+              <img src="/images/svgs/logo.svg" alt="logo" />
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Form className=" w-100 ">
                   <Form.Control
                     type="search"
                     placeholder="Search Drive"
-                    className="mr-sm-2 rounded-5 search-box-width py-2 "
+                    className=" rounded-5 py-2 "
                   />
                 </Form>
-                </Nav>
-                <div className="d-flex align-items-center mt-lg-0 mt-1  ">
-                  <h6 className='m-2 fw-bold '>{authContext.userInfos.username}</h6>
-                  <FaCircleUser className=' text-primary fs-1 '/>
-                </div>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+              </Nav>
+            </Nav>
+            <div className="d-flex align-items-center mt-lg-0 mt-3  ">
+              <FaCircleUser className=' text-primary fs-1 ' />
+              <h6 className='m-2 fw-bold '>{authContext.userInfos.username}</h6>
+              <Button variant='outline-danger' size='sm' className=' ms-2' onClick={logOut}>Logout</Button>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   )
 }
