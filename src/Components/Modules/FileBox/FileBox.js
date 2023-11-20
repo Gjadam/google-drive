d7e6f4a8f6c63988642b8c1e3c351e506d96f09f
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-21 00:46:39 +0330

    delete text-black from FileBox title

--- src/Components/Modules/FileBox/FileBox.js
+++ src/Components/Modules/FileBox/FileBox.js
@@ -6,7 +6,7 @@ export default function FileBox() {
     <div className='file-box__size d-flex justify-content-center align-items-center flex-column p-3 rounded-5 '>
         <img src="/images/svgs/logo.svg" className='file-box-image' alt="image" />
         <div className="file-box__content d-flex justify-content-between w-100">
-            <h6 className=' text-black '>file.pdf</h6>
+            <h6 >file.pdf</h6>
             <TbPdf className=' bg-primary text-white rounded-3 px-1 fs-3 '/>
         </div>
     </div>
