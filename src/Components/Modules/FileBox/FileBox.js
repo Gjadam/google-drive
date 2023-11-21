a65250f99c2ec89b3cdf2eef91bfc2835449755a
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-21 14:32:22 +0330

    add border for fileBox

--- src/Components/Modules/FileBox/FileBox.js
+++ src/Components/Modules/FileBox/FileBox.js
@@ -3,7 +3,7 @@ import './FileBox.css'
 import { TbPdf } from "react-icons/tb";
 export default function FileBox() {
   return (
-    <div className='file-box__size d-flex justify-content-center align-items-center flex-column p-3 rounded-5 '>
+    <div className='file-box__size d-flex justify-content-center align-items-center flex-column p-3 rounded-5 border'>
         <img src="/images/svgs/logo.svg" className='file-box-image' alt="image" />
         <div className="file-box__content d-flex justify-content-between w-100">
             <h6 >file.pdf</h6>
