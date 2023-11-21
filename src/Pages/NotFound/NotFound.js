1c4c7bb1abb7526922517cf1853fbfba8b405b64
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-21 23:25:45 +0330

    create not found page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import './NotFound.css'
+import { Button, Container } from 'react-bootstrap'
+import NavBar from '../../Components/Templates/NavBar/NavBar'
+import { FaGoogleDrive } from "react-icons/fa6";
+import { Link } from 'react-router-dom'
+import PageStyle from '../../Components/Modules/PageStyle/PageStyle';
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <NavBar />
+      <PageStyle />
+      <Container>
+        <div className="d-flex justify-content-center align-items-center flex-column">
+          <img src="/images/png/not-found.png" className='not-found__image-width my-5 ' alt="not-found" />
+          <Link to="/fast-drive" className='text-decoration-none '>
+            <Button className=' rounded-5 px-4 d-flex justify-content-center align-items-center ' variant='light' size='lg'><FaGoogleDrive className=' me-2 text-primary ' />Go to Drive</Button>
+          </Link>
+        </div>
+      </Container>
+    </>
   )
 }
