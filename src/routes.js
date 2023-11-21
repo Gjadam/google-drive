a07e08cd8624070c5246ec9a4569bc77e1c357bc
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-21 22:20:30 +0330

    add FolderInfo route

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import FileInfo from "./Pages/FileInfo/FileInfo"
+import FolderInfo from "./Pages/FolderInfo/FolderInfo"
 import Index from "./Pages/Index/Index"
 import LandingPage from "./Pages/LandingPage/LandingPage"
 import Login from "./Pages/Login/Login"
@@ -10,6 +11,7 @@ const routes = [
     { path: '/', element: <LandingPage />},
     { path: '/fast-drive', element: <Index />},
     { path: '/file-info/:fileName', element: <FileInfo /> },
+    { path: '/folder-info/:folderID', element: <FolderInfo /> },
     { path: '/sign-up', element: <SignUp /> },
     { path: '/login', element: <Login /> },
     { path: '*', element: <NotFound /> },
