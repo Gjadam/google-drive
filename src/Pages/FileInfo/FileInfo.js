57565689b346d3407607e42f6007d164e4122697
DESKTOP-KBCTF04\Elecomp <mohammadjavadbabaei2020@gmail.com>
2023-11-23 18:34:36 +0330

    change fileInfo styles and create logic

--- src/Pages/FileInfo/FileInfo.js
+++ src/Pages/FileInfo/FileInfo.js
@@ -3,23 +3,39 @@ import { Button, Card, Container, Image } from 'react-bootstrap'
 import './FileInfo.css'
 import NavBar from '../../Components/Templates/NavBar/NavBar'
 import PageStyle from '../../Components/Modules/PageStyle/PageStyle'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useState } from 'react'
 export default function FileInfo() {
+
+    const [fileData, setFileData] = useState([])
+    const { fileID } = useParams()
+    const localStorageData = JSON.parse(localStorage.getItem("user"))
+
+    useEffect(() => {
+        fetch(`http://fastdrive.pythonanywhere.com/api/media/${fileID}/`, {
+            headers: {
+                'Authorization': `Token ${localStorageData.token}`
+            }
+        })
+            .then(res => res.json())
+            .then(fileDatas => {
+                setFileData(fileDatas.file)
+            })
+    }, [])
+
     return (
         <Container >
             <NavBar />
             <PageStyle />
-            <Card className=' my-5 '>
-                <Card.Img variant="top" className='card-mage__height' src="/images/svgs/logo.svg" />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer className=' d-flex justify-content-between '>
-                    <Button >Download</Button>
-                    <Button variant='danger'>Delete</Button>
+            <Card className=' my-5 rounded-5 overflow-hidden'>
+                <div className=" d-flex justify-content-center align-items-center ">
+                    <Card.Img variant="top" className='card-image__size' src={fileData} />
+                </div>
+                <Card.Footer className=' w-100 '>
+                    <a href={fileData}>
+                        <Button className=' rounded-bottom-5 w-100 '>Download</Button>
+                    </a>
                 </Card.Footer>
             </Card>
         </Container>
