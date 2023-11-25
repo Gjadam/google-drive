57b104b30c241a026f11619944e8d539eacc3e4d
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-11-25 22:43:53 +0330

    .

--- src/Components/Modules/FileBox/FileBox.js
+++ src/Components/Modules/FileBox/FileBox.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './FileBox.css'
 import { FaRegImage } from "react-icons/fa6";
-export default function FileBox({ fileName, format }) {
+export default function FileBox({ fileName, format, fileImg }) {
   return (
-    <div className='file-box__size d-flex justify-content-center align-items-center flex-column p-3 rounded-5 border'>
-      <img src="/images/svgs/logo.svg" className='file-box-image' alt="image" />
-      <div className="file-box__content d-flex justify-content-between w-100">
-        <span >{fileName.slice(0, 10)}...</span>
+    <div className='file-box__size d-flex justify-content-center align-items-center flex-column rounded-5 border overflow-hidden '>
+      <div className=" overflow-hidden ">
+      <img src={fileImg} className=' w-100 ' alt="image" />
+      </div>
+      
+      <div className="file-box__content d-flex justify-content-between w-100  p-3  border-top ">
+        <span>{fileName.slice(0, 10)}...</span>
         {
           format === "svg" || "jpg" || "png" ? <FaRegImage className=' px-1 fs-3 ' /> : null
         }
