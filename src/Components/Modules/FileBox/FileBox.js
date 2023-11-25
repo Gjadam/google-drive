fd20a3c6f1a4b83b9672868bf1db46aef16748c2
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-11-25 21:31:49 +0330

    change FileBox style

--- src/Components/Modules/FileBox/FileBox.js
+++ src/Components/Modules/FileBox/FileBox.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './FileBox.css'
-import { TbPdf } from "react-icons/tb";
-export default function FileBox() {
+import { FaRegImage } from "react-icons/fa6";
+export default function FileBox({ fileName, format }) {
   return (
     <div className='file-box__size d-flex justify-content-center align-items-center flex-column p-3 rounded-5 border'>
-        <img src="/images/svgs/logo.svg" className='file-box-image' alt="image" />
-        <div className="file-box__content d-flex justify-content-between w-100">
-            <h6 >file.pdf</h6>
-            <TbPdf className=' bg-primary text-white rounded-3 px-1 fs-3 '/>
-        </div>
+      <img src="/images/svgs/logo.svg" className='file-box-image' alt="image" />
+      <div className="file-box__content d-flex justify-content-between w-100">
+        <span >{fileName.slice(0, 10)}...</span>
+        {
+          format === "svg" || "jpg" || "png" ? <FaRegImage className=' px-1 fs-3 ' /> : null
+        }
+      </div>
     </div>
   )
 }
