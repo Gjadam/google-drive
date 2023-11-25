e8a2474bed11a0476e9da7585d0086b9c0cca6b4
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-11-25 23:16:02 +0330

    .

--- src/Pages/FileInfo/FileInfo.js
+++ src/Pages/FileInfo/FileInfo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Card, Container, Image } from 'react-bootstrap'
+import './FileInfo.css'
 import NavBar from '../../Components/Templates/NavBar/NavBar'
 import PageStyle from '../../Components/Modules/PageStyle/PageStyle'
 import { useParams } from 'react-router-dom'
@@ -38,30 +39,33 @@ export default function FileInfo() {
         <Container >
             <NavBar />
             <PageStyle />
+
             {
                 isLoading ? (
                     <Loader />
                 ) : (
-                    <div className="row  my-5 overflow-hidden">
-                        <div className=" col-12 col-md-6 ">
-                            <img className=' w-100  rounded-5 ' src={fileUrl} />
-                        </div>
-                        <div className="col mt-4 mt-md-0 ">
-                            <div className=" d-flex justify-content-between align-items-center p-2 ">
-                                <label className=' text-primary'>name: </label>
-                                <h5 className='  '>{fileName}</h5>
+                    <div className=" d-flex justify-content-center align-items-center FileInfo-height ">
+                        <div className="row overflow-hidden">
+                            <div className=" col-12 col-md-6 ">
+                                <img className=' w-100  rounded-5 ' src={fileUrl} />
                             </div>
-                            <div className=" d-flex justify-content-between align-items-center p-2">
-                                <label className=' text-primary '>format: </label>
-                                <h5 className='  '>{fileFormat}</h5>
+                            <div className="col mt-4 mt-md-0 ">
+                                <div className=" d-flex justify-content-between align-items-center p-2 ">
+                                    <label className=' text-primary'>name: </label>
+                                    <h5 className='  '>{fileName}</h5>
+                                </div>
+                                <div className=" d-flex justify-content-between align-items-center p-2">
+                                    <label className=' text-primary '>format: </label>
+                                    <h5 className='  '>{fileFormat}</h5>
+                                </div>
+                                <div className=" d-flex justify-content-between align-items-center p-2 ">
+                                    <label className=' text-primary '>size: </label>
+                                    <h5 className=' '>{fileSize}</h5>
+                                </div>
+                                <a href={fileUrl}>
+                                    <Button className=' w-100 rounded-5'>Download</Button>
+                                </a>
                             </div>
-                            <div className=" d-flex justify-content-between align-items-center p-2 ">
-                                <label className=' text-primary '>size: </label>
-                                <h5 className=' '>{fileSize}</h5>
-                            </div>
-                            <a href={fileUrl}>
-                                <Button className=' w-100 rounded-5'>Download</Button>
-                            </a>
                         </div>
                     </div>
                 )
