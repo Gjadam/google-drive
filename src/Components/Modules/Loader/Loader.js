a758cff049bee9dcc9f3c2e4991aec4a9686e9f6
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-11-25 21:32:01 +0330

    create Loader

---
+++ src/Components/Modules/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function Loader() {
+    return (
+        <div class="loader-page d-flex justify-content-center align-items-center ">
+            <div class="loading">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
