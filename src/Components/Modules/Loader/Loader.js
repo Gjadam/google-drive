89de0f4e0957c1e9c340fb71debf8b8a76c94cfc
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-11-25 21:49:31 +0330

    change loader height

--- src/Components/Modules/Loader/Loader.js
+++ src/Components/Modules/Loader/Loader.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './Loader.css'
 export default function Loader() {
     return (
-        <div class="loader-page d-flex justify-content-center align-items-center ">
+        <div class="loader-height d-flex justify-content-center align-items-center ">
             <div class="loading">
                 <span></span>
                 <span></span>
