294061f0f7d6eb5d94f3648762b3c0c5897cf821
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-01 20:21:37 +0330

    add QueryClient

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools"
 import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -9,13 +11,19 @@ import './Styles/fonts.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 AOS.init();
+
+const client = new QueryClient()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <QueryClientProvider client={client}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+    <ReactQueryDevtools/>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
