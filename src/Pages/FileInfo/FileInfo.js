50d42a40e511a40627963028a1153023695c2870
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-02 10:11:25 +0330

    use react-query in FileInfo page

--- src/Pages/FileInfo/FileInfo.js
+++ src/Pages/FileInfo/FileInfo.js
@@ -1,45 +1,38 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { Button, Card, Container, Image } from 'react-bootstrap'
 import './FileInfo.css'
 import NavBar from '../../Components/Templates/NavBar/NavBar'
 import PageStyle from '../../Components/Modules/PageStyle/PageStyle'
 import { useParams } from 'react-router-dom'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { RiDownloadCloud2Fill } from "react-icons/ri";
 import Loader from '../../Components/Modules/Loader/Loader'
+import { useQuery } from 'react-query'
 export default function FileInfo() {
 
-    const [fileUrl, setFileUrl] = useState([])
-    const [fileName, setFileName] = useState([])
-    const [fileFormat, setFileFormat] = useState([])
-    const [fileSize, setFileSize] = useState([])
 
     const [isLoading, setIsLoading] = useState(true)
     const { fileID } = useParams()
     const localStorageData = JSON.parse(localStorage.getItem("user"))
 
-    useEffect(() => {
-        fetch(`http://fastdrive.pythonanywhere.com/api/media/${fileID}/`, {
+    // Get Single File
+    const { data: fileData } = useQuery(["single-file", fileID], () => {
+        return fetch(`http://fastdrive.pythonanywhere.com/api/media/${fileID}/`, {
             headers: {
                 'Authorization': `Token ${localStorageData.token}`
             }
         })
             .then(res => res.json())
-            .then(fileDatas => {
-                setFileUrl(fileDatas.file)
-                setFileFormat(fileDatas.file_format)
-                setFileName(fileDatas.file_name)
-                setFileSize(fileDatas.file_size)
-                setIsLoading(false)
-            })
-    }, [])
-
+    },
+    {
+        onSuccess: () => {
+            setIsLoading(false)
+        }
+    })
 
     return (
         <Container >
             <NavBar />
             <PageStyle />
-
             {
                 isLoading ? (
                     <Loader />
@@ -47,23 +40,23 @@ export default function FileInfo() {
                     <div className="FileInfo-height  d-flex justify-content-center align-items-center ">
                         <div className="row overflow-hidden">
                             <div className="FileInfo__image-height  col-12 col-md-6 ">
-                                <img className=' w-100 rounded-5 ' src={fileUrl} />
+                                <img className=' w-100 rounded-5 ' src={fileData?.file} />
                             </div>
-                            <div className="col ">
+                            <div className="col mt-3 mt-md-0  ">
                                 <div className=" d-flex justify-content-between align-items-center p-2 ">
                                     <label className=' text-primary'>name: </label>
-                                    <h5 className='  '>{fileName.slice(0, 20)}...</h5>
+                                    <h5 className='  '>{fileData?.file_name.slice(0, 20)}...</h5>
                                 </div>
                                 <div className=" d-flex justify-content-between align-items-center p-2">
                                     <label className=' text-primary '>format: </label>
-                                    <h5 className='  '>{fileFormat}</h5>
+                                    <h5 className='  '>{fileData?.file_format}</h5>
                                 </div>
                                 <div className=" d-flex justify-content-between align-items-center p-2 ">
                                     <label className=' text-primary '>size: </label>
-                                    <h5 className=' '>{fileSize}</h5>
+                                    <h5 className=' '>{fileData?.file_size}</h5>
                                 </div>
-                                <a href={fileUrl}>
-                                    <Button className=' w-100 rounded-5'>Download</Button>
+                                <a href={fileData?.file} className=' text-decoration-none '>
+                                    <Button className=' d-flex justify-content-center  align-items-center   w-100 rounded-5 '><RiDownloadCloud2Fill className='fs-5  me-1 ' />Download</Button>
                                 </a>
                             </div>
                         </div>
