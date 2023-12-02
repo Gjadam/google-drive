4fc89d88a1f54dca3fa8715b464daecbaa7fcd47
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-02 10:23:35 +0330

    add react-query

--- src/Pages/FolderInfo/FolderInfo.js
+++ src/Pages/FolderInfo/FolderInfo.js
@@ -3,38 +3,41 @@ import { Container } from 'react-bootstrap'
 import NavBar from '../../Components/Templates/NavBar/NavBar'
 import PageStyle from '../../Components/Modules/PageStyle/PageStyle'
 import SectionHeader from '../../Components/Modules/SectionHeader/SectionHeader'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import FileBox from '../../Components/Modules/FileBox/FileBox'
+import { useQuery } from 'react-query'
 
 export default function FolderInfo() {
 
-    const [folderData, setFolderData] = useState([])
 
     const { folderID } = useParams()
     const localStorageData = JSON.parse(localStorage.getItem("user"))
 
-    useEffect(() => {
-        // Get Single Folder From Server
-        fetch(`http://fastdrive.pythonanywhere.com/api/folders/${folderID}/`, {
+    // Get Single Folder
+    const { data: folderData } = useQuery("single-folder", () => {
+        return fetch(`http://fastdrive.pythonanywhere.com/api/folders/${folderID}/`, {
             headers: {
                 'Authorization': `Token ${localStorageData.token}`
             }
         })
             .then(res => res.json())
-            .then(folderDatas => {
-                setFolderData(folderDatas)
-            })
-    }, [])
-    
+    })
+
     return (
         <Container>
             <NavBar />
             <PageStyle />
-            <SectionHeader title={folderData.title} />
+            <SectionHeader title={folderData?.title} />
+            <div className="d-flex align-items-center">
+                <form className=' ' method="POST" encType="multipart/form-data">
+                    <label htmlFor="files" className='btn btn-primary d-flex align-items-center '>Upload file</label>
+                    <input id="files" className='d-none' type="file" />
+                </form>
+            </div>
+
             <div className=" d-flex justify-content-around align-items-centerf flex-wrap gap-3 mt-4  ">
-                <Link to="/file-info/1" className='text-decoration-none '>
+                {/* <Link to="/file-info/1" className='text-decoration-none '>
                     <FileBox />
                 </Link>
                 <Link to="/file-info/1" className='text-decoration-none '>
@@ -45,7 +48,7 @@ export default function FolderInfo() {
                 </Link>
                 <Link to="/file-info/1" className='text-decoration-none '>
                     <FileBox />
-                </Link>
+                </Link> */}
 
             </div>
         </Container>
