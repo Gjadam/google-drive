c34a342f9b1b1f8100321b9f2704c655e086d944
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2023-12-09 02:21:49 +0330

    .

--- src/Components/Modules/FileBox/FileBox.js
+++ src/Components/Modules/FileBox/FileBox.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './FileBox.css'
 import { FaRegImage } from "react-icons/fa6";
+import { FaFilePdf } from "react-icons/fa6";
+import { PiVideoFill } from "react-icons/pi";
 export default function FileBox({ fileName, format, fileImg }) {
+
+  const [fileIcon, setFileIcon] = useState("")
+  const [fileImage, setFileImage] = useState("")
+
+  useEffect(() => {
+
+    switch (format) {
+      case "pdf":
+        setFileIcon(<FaFilePdf className=' px-1 fs-3 ' />)
+        setFileImage(<img src='/images/jpg/pdf-image.jpeg' className=' w-100   ' alt="image" />)
+        break
+      case "jpg":
+        setFileIcon(<FaRegImage className=' px-1 fs-3 ' />)
+        setFileImage(<img src={fileImg} className=' w-100' alt="image" />)
+        break
+      case "svg":
+        setFileIcon(<FaRegImage className=' px-1 fs-3 ' />)
+        setFileImage(<img src={fileImg} className=' w-100 ' alt="image" />)
+        break
+      case "png":
+        setFileIcon(<FaRegImage className=' px-1 fs-3 ' />)
+        setFileImage(<img src={fileImg} className=' w-100 ' alt="image" />)
+        break
+      case "mp4":
+        setFileIcon(<PiVideoFill className=' px-1 fs-2 ' />)
+        setFileImage(<img src='/images/jpg/video-image.jpg' className=' w-100   ' alt="image" />)
+        break
+    }
+  }, [])
+
   return (
     <div className='file-box__size d-flex justify-content-between align-items-center flex-column rounded-5 border overflow-hidden shadow mb-3 '>
       <div className=" overflow-hidden ">
-      <img src={fileImg} className=' w-100 ' alt="image" />
+          {fileImage}
       </div>
-      
-      <div className="file-box__content d-flex justify-content-between w-100  p-3  border-top ">
+      <div className="file-box__content d-flex justify-content-between w-100  px-3 py-2   border-top ">
         <span>{fileName.slice(0, 10)}...</span>
-        {
-          format === "svg" || "jpg" || "png" ? <FaRegImage className=' px-1 fs-3 ' /> : null
-        }
+        {fileIcon}
       </div>
     </div>
   )
--- src/Pages/FileInfo/FileInfo.js
+++ src/Pages/FileInfo/FileInfo.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 import { Button, Card, Container, Image } from 'react-bootstrap'
 import './FileInfo.css'
 import NavBar from '../../Components/Templates/NavBar/NavBar'
@@ -8,8 +8,9 @@ import { RiDownloadCloud2Fill } from "react-icons/ri";
 import Loader from '../../Components/Modules/Loader/Loader'
 import { useQuery } from 'react-query'
 export default function FileInfo() {
-
-
+    
+    
+    const [fileImage, setFileImage] = useState("")
     const [isLoading, setIsLoading] = useState(true)
     const { fileID } = useParams()
     const localStorageData = JSON.parse(localStorage.getItem("user"))
@@ -29,6 +30,29 @@ export default function FileInfo() {
         }
     })
 
+
+
+    useEffect(() => {
+  
+      switch (fileData?.file_format) {
+        case "pdf":
+          setFileImage(<img src='/images/jpg/pdf-image.jpeg' className=' w-100  rounded-5  ' alt="image" />)
+          break
+        case "jpg":
+          setFileImage(<img src={fileData?.file} className=' w-100 rounded-5' alt="image" />)
+          break
+        case "svg":
+          setFileImage(<img src={fileData?.file} className=' w-100 rounded-5 ' alt="image" />)
+          break
+        case "png":
+          setFileImage(<img src={fileData?.file} className=' w-100 rounded-5 ' alt="image" />)
+          break
+        case "mp4":
+          setFileImage(<img src='/images/jpg/video-image.jpg' className=' w-100 rounded-5   ' alt="image" />)
+          break
+      }
+})
+
     return (
         <Container >
             <NavBar />
@@ -40,7 +64,7 @@ export default function FileInfo() {
                     <div className="FileInfo-height  d-flex justify-content-center align-items-center ">
                         <div className="row overflow-hidden">
                             <div className="FileInfo__image-height  col-12 col-md-6 ">
-                                <img className=' w-100 rounded-5 ' src={fileData?.file} />
+                                {fileImage}
                             </div>
                             <div className="col mt-3 mt-md-0  ">
                                 <div className=" d-flex justify-content-between align-items-center p-2 ">
