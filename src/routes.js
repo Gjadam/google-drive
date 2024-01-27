23f72af8978620c3cc769db3322bb101721f471a
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-01-27 18:49:45 +0330

    add Search route

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import Index from "./Pages/Index/Index"
 import LandingPage from "./Pages/LandingPage/LandingPage"
 import Login from "./Pages/Login/Login"
 import NotFound from "./Pages/NotFound/NotFound"
+import Search from "./Pages/Search/Search"
 import SignUp from "./Pages/SignUp/SignUp"
 
 
 const routes = [
     { path: '/', element: <LandingPage />},
+    { path: '/search/:searchID', element: <Search />},
     { path: '/fast-drive', element: <Index />},
     { path: '/file-info/:fileID', element: <FileInfo /> },
     { path: '/folder-info/:folderID', element: <FolderInfo /> },
